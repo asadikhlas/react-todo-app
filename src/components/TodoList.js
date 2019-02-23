821031939443285d14ef95643ea55378bc9e2b2c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-23 18:22:30 +0500

    commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import TodoItem from './Todoitem';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        hello from todo list
+      </div>
+    )
+  }
+}
